6910d31de3cf158b3d923460fafd2235936acdfc
ptaylor <paul.e.taylor@me.com>
2022-11-15 23:14:42 -0800

    update llvm test

--- test/llvm/test.sh
+++ test/llvm/test.sh
@@ -14,7 +14,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" grep 'llvm-toolchain-$(lsb_release -cs) main' /etc/apt/sources.list{,.d/*.list}
+check "version" grep "llvm-toolchain-$(lsb_release -cs) main" /etc/apt/sources.list{,.d/*.list}
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.

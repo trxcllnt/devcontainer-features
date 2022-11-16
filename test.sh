09a077f10699db739a808ee3575864406a3c4d57
ptaylor <paul.e.taylor@me.com>
2022-11-15 22:55:19 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@
 
 image=mcr.microsoft.com/devcontainers/base:ubuntu
 
-# for feature in \
-#                `# cuda`  \
-#                llvm  \
-#                `# nvhpc` ; do
-#     npx --package=@devcontainers/cli -c "devcontainer features test -f $feature -i $image .";
-# done
+for feature in \
+               `# cuda`  \
+               `# llvm`  \
+               nvhpc ; do
+npx --package=@devcontainers/cli -c "\
+    devcontainer features test \
+        --skip-scenarios --log-level trace -f $feature -i $image -p .";
+done
 
-npx --package=@devcontainers/cli -c "devcontainer features test --global-scenarios-only";
+npx --package=@devcontainers/cli -c "devcontainer features test --log-level trace --global-scenarios-only";

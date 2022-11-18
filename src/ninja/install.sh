388f728f40865418226705279c2bb9fb03e2031a
ptaylor <paul.e.taylor@me.com>
2022-11-18 11:16:58 -0800

    install unzip

--- src/ninja/install.sh
+++ src/ninja/install.sh
@@ -25,7 +25,7 @@ if dpkg -s ninja-build > /dev/null 2>&1; then
     apt-get autoremove -y;
 fi
 
-check_packages wget ca-certificates
+check_packages wget unzip ca-certificates
 
 echo "Installing ninja-build...";
 

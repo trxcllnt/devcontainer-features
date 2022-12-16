417215f80713e73ddf959edef64d3c490d163ace
ptaylor <paul.e.taylor@me.com>
2022-12-16 01:16:04 -0800

    only install gds-tools on x86

--- src/cuda/install.sh
+++ src/cuda/install.sh
@@ -44,11 +44,15 @@ apt-get update;
 echo "Installing minimal CUDA toolkit..."
 
 cuda_ver=${CUDAVERSION/./-}
+gds_tools=
+if [[ $(uname -p) == x86_64 ]]; then
+    gds_tools="gds-tools-${cuda_ver}";
+fi
 
 DEBIAN_FRONTEND=noninteractive              \
 apt-get install -y --no-install-recommends  \
     libnccl-dev                             \
-    gds-tools-${cuda_ver}                   \
+    ${gds_tools}                            \
     cuda-compat-${cuda_ver}                 \
     cuda-nvml-dev-${cuda_ver}               \
     cuda-compiler-${cuda_ver}               \

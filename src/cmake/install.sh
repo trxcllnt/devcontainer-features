d295a0341a66946652cb5407c14238726e730693
ptaylor <paul.e.taylor@me.com>
2023-01-18 19:41:57 -0800

    install pkg-config

--- src/cmake/install.sh
+++ src/cmake/install.sh
@@ -25,7 +25,7 @@ if dpkg -s cmake > /dev/null 2>&1; then
     apt-get autoremove -y;
 fi
 
-check_packages wget ca-certificates bash-completion
+check_packages wget ca-certificates bash-completion pkg-config
 
 echo "Downloading CMake...";
 
